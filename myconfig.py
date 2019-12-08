2f81aa266adcd756e3396995169dcb0a77369b46
Your Name <you@example.com>
2019-12-08 09:20:10 +0000

    data5

--- myconfig.py
+++ myconfig.py
@@ -46,7 +46,7 @@ STEERING_RIGHT_PWM = 305        #pwm value for full right steering
 # 
 # #THROTTLE
 # THROTTLE_CHANNEL = 0            #channel on the 9685 pwm board 0-15
-THROTTLE_FORWARD_PWM = 480      #pwm value for max forward throttle
+THROTTLE_FORWARD_PWM = 430      #pwm value for max forward throttle
 THROTTLE_STOPPED_PWM = 343      #pwm value for no movement
 THROTTLE_REVERSE_PWM = 260      #pwm value for max reverse throttle
 # 
